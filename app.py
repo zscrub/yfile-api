034683b759d1db924e117bf6b563910da1ddfb8d
zscrub <rubinzack40@gmail.com>
2021-08-10 03:58:17 -0400

    added route to generate token, stores in JSON file

--- app.py
+++ app.py
@@ -1,20 +1,38 @@
 import os
-from fastapi import FastAPI, File, UploadFile
+from json import dump, load
+from random import choice
 from pydantic import BaseModel
+from string import ascii_letters
 from fastapi.responses import FileResponse
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI, File, UploadFile, HTTPException
 
 UPLOAD_DIRECTORY = "uploaded_files"
+password = "password"
+
+with open("db.json", "r") as f:
+    JSONtoken = load(f)["token"]
+
+class Auth(BaseModel):
+    password: str
+
+class Token(BaseModel):
+    token: str
+
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
 app = FastAPI()
 
+# index
 @app.get("/")
 async def home():
     return "yfile-api"
 
+# return a list of files from upload_directory
 @app.get("/files")
+# need to add a token to header
 async def list_files():
     """Endpoint to list files on the server."""
     files = []
@@ -24,22 +42,40 @@ async def list_files():
             files.append(filename)
     return files
 
+
 # download a file 
 @app.get("/files/{filename}")
-async def get_file(filename: str):
+# token needs to be in header
+async def get_file(token: Token, filename: str):
     # media_type and filename attributes are optional
-    return FileResponse(path=UPLOAD_DIRECTORY+'/{0}'.format(filename), headers = {"Content-Disposition": "attachment; filename=" + filename})
-
-#upload a file
-@app.post("/files/upload")
-async def create_upload_file(file: UploadFile = File(...)):
-    if '/' not in file.filename:
-        with open(os.path.join(UPLOAD_DIRECTORY, file.filename), "wb") as fp:
-            fp.write(await file.read())
-        return 201
+    if token.token == JSONtoken:
+        return FileResponse(path=UPLOAD_DIRECTORY+'/'+filename, headers = {"Content-Disposition": "attachment; filename="+filename})
     else:
-        os.abort(400, 'References to subdirectories not allowed')
+        raise HTTPException(status_code=401, detail="Incorrect token")
 
+# upload a file
+@app.post("/files/upload")
+# need to pass token in header instead of body (same for other routes)
+async def create_upload_file(file: UploadFile = File(...)):
+        # if token.token == JSONtoken:
+            if '/' not in file.filename:
+                with open(os.path.join(UPLOAD_DIRECTORY, file.filename), "wb") as fp:
+                    fp.write(await file.read())
+                return 201
+            else:
+                raise HTTPException(status_code=400, detail='References to subdirectories not allowed')
+
+# route to generate a uuid
+@app.post("/token")
+def generate_token(login: Auth):
+    if login.password == password:
+        chars = ascii_letters
+        token = ''.join(choice(chars) for _ in range(32)) 
+        with open("db.json", "w") as fp:
+            dump({"token": token}, fp)
+        return token
+    else:
+        raise HTTPException(status_code=401, detail='Incorrect password')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
